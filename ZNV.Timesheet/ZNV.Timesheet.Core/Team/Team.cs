dff44fe6ba8f235156af810f73915da213f19644
liangguo85 <lg3605119@126.com>
2019-07-03 09:41:54 +0800

    增加表单验证功能

--- ZNV.Timesheet/ZNV.Timesheet.Core/Team/Team.cs
+++ ZNV.Timesheet/ZNV.Timesheet.Core/Team/Team.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ZNV.Timesheet.Team
@@ -8,13 +9,18 @@ namespace ZNV.Timesheet.Team
     [Table("Team")]
     public class Team : Entity<int>, IHasCreationTime
     {
+        [Required(ErrorMessage = "科室名称不能为空!")]
         public virtual string TeamName { get; set; }
 
+        [Required(ErrorMessage = "所属部门不能为空!")]
         public virtual string DepartmentID { get; set; }
+
         [NotMapped]
         public virtual string DepartmentName { get; set; }
 
+        [Required(ErrorMessage = "科室领导不能为空!")]
         public virtual string TeamLeader { get; set; }
+
         [NotMapped]
         public virtual string TeamLeaderName { get; set; }
 

4fdceac87c2d7a18ae8d1002cff37e5c647fa92a
KojarLiu <wiwijia_007@163.com>
2019-07-22 10:39:11 +0800

    我的周报、我的草稿、我的提交3个界面合并成一个我的周报，我的周报增加提交按钮，其他调整

---
+++ ZNV.Timesheet/ZNV.Timesheet.Web/Common/CommonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZNV.Timesheet.Web.Common
+{
+    public class CommonHelper
+    {
+        public static string GetProjectNameByProjectID(List<Project.Project> projects, int projectID)
+        {
+            if (projects != null && projects.Count > 0)
+            {
+                var w = projects.Where(p => p.Id == projectID).ToList();
+                if (w.Count > 0)
+                {
+                    return w[0].ProjectName;
+                }
+            }
+            return "";
+        }
+    }
+}
--- ZNV.Timesheet/ZNV.Timesheet.Web/Controllers/TimesheetPendingController.cs
+++ ZNV.Timesheet/ZNV.Timesheet.Web/Controllers/TimesheetPendingController.cs
@@ -10,10 +10,13 @@ namespace ZNV.Timesheet.Web.Controllers
     public class TimesheetPendingController : Controller
     {
         private readonly ITimesheetAppService _appService;
+        private readonly Project.IProjectAppService _projectService;
         public TimesheetPendingController(ITimesheetAppService appService, Project.IProjectAppService projectService)
         {
             _appService = appService;
+            _projectService = projectService;
         }
+
         // GET: Timesheet
         public ActionResult Index()
         {
@@ -25,12 +28,70 @@ namespace ZNV.Timesheet.Web.Controllers
         {
             int start = Convert.ToInt32(Request["start"]);
             int length = Convert.ToInt32(Request["length"]);
-            var list = _appService.GetAllTimesheetsByUser(user, null, null).Where(ts=>ts.Status == "Pending").ToList();
-            int totalRow = list.Count;
-            list = list.Skip(start).Take(length).ToList();
-            return Json(new { data = list, draw = Request["draw"], recordsTotal = totalRow, recordsFiltered = totalRow }, JsonRequestBehavior.AllowGet);
+            var list = _appService.GetAllTimesheetsByUser(user, null, null).Where(ts => ts.Status == ApproveStatus.Approving).ToList();
+            List<Project.Project> projects = _projectService.GetAllProjectList();
+            foreach (var ts in list)
+            {
+                ts.ProjectName = Common.CommonHelper.GetProjectNameByProjectID(projects, ts.ProjectID.Value);
+            }
+            var listGroup = list.GroupBy(ts => new { ts.TimesheetUser, ts.WorkflowInstanceID })
+                .Select(g => new TimesheetForPending()
+                {
+                    TimesheetUser = g.Key.TimesheetUser,
+                    WorkflowInstanceID = g.Key.WorkflowInstanceID,
+                    ProjectName = List2String(g.Select(ts => ts.ProjectName).Distinct().ToList()),
+                    TimesheetDate = List2String(g.Select(ts => ts.TimesheetDate.Value.ToString("yyyy-MM-dd")).Distinct().ToList()),
+                    WorkContent = List2String(g.Select(ts => ts.WorkContent).Distinct().ToList()),
+                    Workload = g.Sum(ts => ts.Workload),
+                    Remarks = List2String(g.Select(ts => ts.Remarks).Distinct().ToList()),
+                    IDList = List2String(g.Select(ts => ts.Id.ToString()).Distinct().ToList()),
+                    TimesheetList = g.ToList()
+                }).ToList();
+
+            int totalRow = listGroup.Count;
+            listGroup = listGroup.Skip(start).Take(length).ToList();
+            return Json(new { data = listGroup, draw = Request["draw"], recordsTotal = totalRow, recordsFiltered = totalRow }, JsonRequestBehavior.AllowGet);
+        }
+
+        private string List2String(List<string> charList)
+        {
+            string list = string.Empty;
+            if (charList != null && charList.Count > 0)
+            {
+                for (int i = 0; i < charList.Count; i++)
+                {
+                    list += string.Format("{0}{1}", (list == "" ? "" : ","), charList[i]);
+                }
+            }
+            return list;
         }
         
+        public string GetRemarkFromTimesheetList(List<Timesheet.Timesheet> tsList)
+        {
+            string remark = string.Empty;
+            if (tsList != null && tsList.Count > 0)
+            {
+                for (int i = 0; i < tsList.Count; i++)
+                {
+                    remark += string.Format("{0}{1}", (remark == "" ? "" : ","), tsList[i].TimesheetDate.Value.ToString("yyyy-MM-dd"));
+                }
+            }
+            return remark;
+        }
+
+        public string GetIDListFromTimesheetList(List<Timesheet.Timesheet> tsList)
+        {
+            string IDList = string.Empty;
+            if (tsList != null && tsList.Count > 0)
+            {
+                for (int i = 0; i < tsList.Count; i++)
+                {
+                    IDList += string.Format("{0}{1}", (IDList == "" ? "" : ","), tsList[i].Id);
+                }
+            }
+            return IDList;
+        }
+
         [HttpPost]
         public ActionResult AddOrEdit(Timesheet.Timesheet ts)
         {
@@ -65,7 +126,7 @@ namespace ZNV.Timesheet.Web.Controllers
                 foreach (var id in idList)
                 {
                     var ts = _appService.GetTimesheetsByID(int.Parse(id));
-                    ts.Status = "Approved";
+                    ts.Status = ApproveStatus.Approved;
                     AddOrEdit(ts);
                 }
                 return Json(new { success = true, message = "审批通过工时数据成功!" }, JsonRequestBehavior.AllowGet);
@@ -77,9 +138,9 @@ namespace ZNV.Timesheet.Web.Controllers
         }
 
         /// <summary>
-        /// 审批拒绝
+        /// 审批驳回
         /// </summary>
-        /// <param name="tsIdList">需要审批拒绝的工时id列表</param>
+        /// <param name="tsIdList">需要审批驳回的工时id列表</param>
         /// <returns></returns>
         [HttpPost]
         public ActionResult CommReject(String tsIdList)
@@ -90,14 +151,14 @@ namespace ZNV.Timesheet.Web.Controllers
                 foreach (var id in idList)
                 {
                     var ts = _appService.GetTimesheetsByID(int.Parse(id));
-                    ts.Status = "Draft";
+                    ts.Status = ApproveStatus.Draft;
                     AddOrEdit(ts);
                 }
-                return Json(new { success = true, message = "审批拒绝工时数据成功!" }, JsonRequestBehavior.AllowGet);
+                return Json(new { success = true, message = "审批驳回工时数据成功!" }, JsonRequestBehavior.AllowGet);
             }
             else
             {
-                return Json(new { success = false, message = "需要审批拒绝的工时数据为空!" }, JsonRequestBehavior.AllowGet);
+                return Json(new { success = false, message = "需要审批驳回的工时数据为空!" }, JsonRequestBehavior.AllowGet);
             }
         }
     }

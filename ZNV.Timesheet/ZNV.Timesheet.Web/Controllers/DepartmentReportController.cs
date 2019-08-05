15a51ec440daaa4bf1043eb735ee82e12e1c810d
KojarLiu <wiwijia_007@163.com>
2019-08-06 01:41:16 +0800

    完成下载excel功能

--- ZNV.Timesheet/ZNV.Timesheet.Web/Controllers/DepartmentReportController.cs
+++ ZNV.Timesheet/ZNV.Timesheet.Web/Controllers/DepartmentReportController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,6 +25,17 @@ namespace ZNV.Timesheet.Web.Controllers
             JSONresult = JsonConvert.SerializeObject(dt);
             //Response.Write(JSONresult);
             return View(dt);
+        }
+
+        public FileResult GetExcelForReport()
+        {
+            DataTable dt = _reportAppService.GetDepartmentReport(DateTime.Now.AddYears(-1), DateTime.Now.AddYears(1));
+            string sheetName = "部门工时统计报表";
+            var book = Common.CommonHelper.CreateHSSFromDataTable("sheetName", dt, new List<int>() { 0,1 });
+            MemoryStream ms = new MemoryStream();
+            book.Write(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+            return File(ms, "application/vnd.ms-excel", sheetName + ".xls");
         }
     }
 }

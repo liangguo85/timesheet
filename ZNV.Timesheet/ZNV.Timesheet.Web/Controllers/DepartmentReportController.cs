0349a54120cff68f8875b13cd23d6cc7d1332743
liangguo85 <lg3605119@126.com>
2019-08-05 19:49:25 +0800

    提交代码

--- ZNV.Timesheet/ZNV.Timesheet.Web/Controllers/DepartmentReportController.cs
+++ ZNV.Timesheet/ZNV.Timesheet.Web/Controllers/DepartmentReportController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using ZNV.Timesheet.Report;
 
 namespace ZNV.Timesheet.Web.Controllers
@@ -19,6 +20,9 @@ namespace ZNV.Timesheet.Web.Controllers
         public ActionResult Index()
         {
             DataTable dt = _reportAppService.GetDepartmentReport(DateTime.Now.AddYears(-1), DateTime.Now.AddYears(1));
+            string JSONresult;
+            JSONresult = JsonConvert.SerializeObject(dt);
+            //Response.Write(JSONresult);
             return View(dt);
         }
     }

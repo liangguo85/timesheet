83a349ab7c185a52c821d0ee85555e72ce527d67
liangguo85 <lg3605119@126.com>
2019-08-10 17:06:40 +0800

    优化代码

---
+++ ZNV.Timesheet/ZNV.Timesheet.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using ZNV.Timesheet.Web.Common;
+
+namespace ZNV.Timesheet.Web.Controllers
+{
+    public class HomeController : TimesheetControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SSOLogOut()
+        {
+            HttpContext.Session["OAUserName"] = "";
+            CommonHelper.CurrentUser = "";
+            return Json(new { success = true, message = "清空Session成功" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}

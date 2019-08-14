a49d05c230387d0ade5c5c4aa2cd663fd69ed4ed
liangguo85 <lg3605119@126.com>
2019-08-14 09:33:07 +0800

    完善Layout 个人信息

---
+++ ZNV.Timesheet/ZNV.Timesheet.Web/Controllers/LayoutController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZNV.Timesheet.Employee;
+
+namespace ZNV.Timesheet.Web.Controllers
+{
+    public class LayoutController : Controller
+    {
+        private readonly IEmployeeAppService _empService;
+
+        public LayoutController(IEmployeeAppService empService)
+        {
+            _empService = empService;
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult EmployeeInfo()
+        {
+            var model = _empService.GetEmployeeByCode(Common.CommonHelper.CurrentUser);
+
+            return PartialView("_EmployeeInfo", model);
+        }
+    }
+}

7837b216ddf2f9f118cb48696a7b50ead39cf929
liangguo85 <lg3605119@126.com>
2019-09-01 09:17:35 +0800

    实现项目编码自动生成

--- ZNV.Timesheet/ZNV.Timesheet.Core/Project/Project.cs
+++ ZNV.Timesheet/ZNV.Timesheet.Core/Project/Project.cs
@@ -8,22 +8,22 @@ namespace ZNV.Timesheet.Project
     [Table("Project")]
     public class Project : BaseEntity
     {
+        [Required(ErrorMessage = "类别不能为空!")]
         public virtual string Category { get; set; }
 
-        [Required(ErrorMessage = "项目代码不能为空!")]
+        [Required(ErrorMessage = "项目编码不能为空!")]
         public virtual string ProjectCode { get; set; }
 
         [Required(ErrorMessage = "科室名称不能为空!")]
         public virtual string ProjectName { get; set; }
 
-        [RequiredIf("Category != '售前售后'", ErrorMessage = "项目经理不能为空!")]
-        [AssertThat("ProjectManagerID != ''", ErrorMessage = "项目经理不能为空!")]
+        [RequiredIf("Category == '立项项目' || Category == '非立项项目'", ErrorMessage = "项目经理不能为空!")]
         public virtual string ProjectManagerID { get; set; }
 
         [NotMapped]
         public virtual string ProjectManagerName { get; set; }
 
-        [RequiredIf("Category != '售前售后'", ErrorMessage = "产品经理不能为空!")]
+        [RequiredIf("Category == '立项项目' || Category == '非立项项目'", ErrorMessage = "产品经理不能为空!")]
         public virtual string ProductManagerID { get; set; }
 
         [NotMapped]
@@ -34,19 +34,19 @@ namespace ZNV.Timesheet.Project
         [NotMapped]
         public virtual string ProductLeaderName { get; set; }
 
-        [RequiredIf("Category != '售前售后'", ErrorMessage = "项目类别不能为空！")]
+        [RequiredIf("Category == '立项项目' || Category == '非立项项目'", ErrorMessage = "项目类别不能为空！")]
         public virtual string ProjectType { get; set; }
 
-        [RequiredIf("Category != '售前售后'", ErrorMessage = "项目级别不能为空")]
+        [RequiredIf("Category == '立项项目' || Category == '非立项项目'", ErrorMessage = "项目级别不能为空")]
         public virtual string ProjectLevel { get; set; }
 
-        [RequiredIf("Category != '售前售后'", ErrorMessage = "项目性质不能为空！")]
+        [RequiredIf("Category == '立项项目' || Category == '非立项项目'", ErrorMessage = "项目性质不能为空！")]
         public virtual string ProjectKind { get; set; }
 
-        [RequiredIf("Category != '售前售后'", ErrorMessage = "产线属性不能为空！")]
+        [RequiredIf("Category == '立项项目' || Category == '非立项项目'", ErrorMessage = "产线属性不能为空！")]
         public virtual string ProductionLineAttribute { get; set; }
 
-        [RequiredIf("Category != '售前售后'", ErrorMessage = "项目状态不能为空！")]
+        [RequiredIf("Category == '立项项目' || Category == '非立项项目'", ErrorMessage = "项目状态不能为空！")]
         public virtual string ProjectStatus { get; set; }
 
         public virtual bool IsEnabled { get; set; }
